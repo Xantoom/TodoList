50c6fe0b8666887bf3603e5eecdd383c40aa8ff1
Xantoom <xantoom@gmail.com>
2025-07-12 02:23:26 +0200

    Project Initialization

---
+++ migrations/Version20250619215702.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250619215702 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE task (id SERIAL NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, title VARCHAR(255) NOT NULL, content TEXT NOT NULL, is_done BOOLEAN DEFAULT NULL, PRIMARY KEY(id))
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN task.created_at IS '(DC2Type:datetime_immutable)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE "user" (id SERIAL NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON "user" (email)
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE SCHEMA public
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE task
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE "user"
+        SQL);
+    }
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+class UserController extends AbstractController
+{
+	#[Route('/users', name: 'user_list')]
+	public function list(UserRepository $userRepository): Response
+	{
+		return $this->render('user/list.html.twig', [
+			'users' => $userRepository->findAll()
+		]);
+	}
+
+	#[Route('/users/create', name: 'user_create')]
+	public function create(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+	{
+		$user = new User();
+		$form = $this->createForm(UserType::class, $user);
+
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			$hashedPassword = $passwordHasher->hashPassword($user, $user->getPassword());
+			$user->setPassword($hashedPassword);
+
+			$entityManager->persist($user);
+			$entityManager->flush();
+
+			$this->addFlash('success', "L'utilisateur a bien été ajouté.");
+
+			return $this->redirectToRoute('user_list');
+		}
+
+		return $this->render('user/create.html.twig', [
+			'form' => $form->createView()
+		]);
+	}
+
+	#[Route('/users/{id}/edit', name: 'user_edit')]
+	public function edit(User $user, Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+	{
+		$form = $this->createForm(UserType::class, $user);
+
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			$hashedPassword = $passwordHasher->hashPassword($user, $user->getPassword());
+			$user->setPassword($hashedPassword);
+
+			$entityManager->flush();
+
+			$this->addFlash('success', "L'utilisateur a bien été modifié");
+
+			return $this->redirectToRoute('user_list');
+		}
+
+		return $this->render('user/edit.html.twig', [
+			'form' => $form->createView(),
+			'user' => $user
+		]);
+	}
+}
